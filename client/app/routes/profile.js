9b1ba924cccdd7edf92c942e72e0308b5000b91a
Simon Olsberg <simon@olsbergfamily.net>
2015-01-31 08:54:08 -0500

    Refactor out the building of the profile from the route into a separate Profile object

--- client/app/routes/profile.js
+++ client/app/routes/profile.js
@@ -1,31 +1,12 @@
 import Ember from 'ember';
+import Profile from 'commons/models/profile';
 
 export default Ember.Route.extend({
   model: function(){
     var store = this.store;
-    return new Ember.RSVP.Promise(function(resolve){
-      //get user model
-      store.find('user', {uid: this.get('session.user.uid')}).then(function(users){
-        var user = users.get('firstObject');
-        Ember.RSVP.hash({
-          responses: user.get('profile_responses'),
-          sections: store.find('profile-section'),
-          questions: store.find('profile-question')
-        }).then(function(results){
-          resolve(results.sections.map(function(section){
-            return {
-              section: section,
-              fields: section.get('questions').map(function(question){
-                return {
-                  question: question,
-                  response: results.responses.findBy('question_id', parseInt(question.get('id'), 10)),
-                  user: user
-                };
-              })
-            };
-          }));
-        });
-      });
-    }.bind(this));
+    return store.find('user', {uid: this.get('session.user.uid')}).then(function(users){
+      var user = users.get('firstObject');
+      return Profile.create({user: user, store: store}).get('sections');
+    });
   }
 });
