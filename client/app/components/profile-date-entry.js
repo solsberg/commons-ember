ed017a8457153c817151746fc030c664bc65a80f
Simon Olsberg <simon@olsbergfamily.net>
2015-04-28 21:01:06 -0400

    Update ember-cli-bootstrap-datepicker - new version uses javascript date values

--- client/app/components/profile-date-entry.js
+++ client/app/components/profile-date-entry.js
@@ -1,6 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  date_value: Ember.computed('value', function(key, value, previousValue){
+    if (arguments.length > 1) {
+      if (value instanceof Date){
+        this.set('value', value.toUTCString());
+      }
+    }
+
+    var text = this.get('value');
+    if (!text || text ===''){
+      return null;
+    }
+    return new Date(text);
+  }),
+
   dateChanged: function(){
     this.sendAction("action", this.get('value'));
   }.observes('value')
