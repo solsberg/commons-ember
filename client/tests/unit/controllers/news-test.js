7800a1a934653204e359b528d9cf37f0d336e4d0
Simon Olsberg <simon@olsbergfamily.net>
2015-05-22 08:05:03 -0400

    Use sinon stubs for store methods

--- client/tests/unit/controllers/news-test.js
+++ client/tests/unit/controllers/news-test.js
@@ -2,64 +2,63 @@ import { test, moduleFor } from 'ember-qunit';
 import Ember from 'ember';
 import DS from 'ember-data';
 
-var user = {
-  uid: 'user'
-};
-
-var new_record = {
-  save: function(){
-    return Ember.RSVP.resolve();
-  }
-};
-
-var store = {
-  find: function(){
-    return Ember.RSVP.resolve([user]);
-  },
-
-  createRecord: function(record_type, values){
-    return new_record;
-  }
-};
-
 moduleFor('controller:news', 'NewsController', {
   // Specify the other units that are required for this test.
   setup: function(){
     var controller = this.subject({auth: Ember.Object.create({})});
-    controller.set('store', store);
+
+    this.store = {};
+    this.new_record = {};
+
+    const user = {
+      uid: 'user'
+    };
+
+    this.new_record.save = sinon.stub();
+    this.new_record.save.returns(Ember.RSVP.resolve());
+
+    this.store.createRecord = sinon.stub();
+    this.store.createRecord.returns(this.new_record);
+
+    this.store.find = sinon.stub();
+    this.store.find.returns(Ember.RSVP.resolve([user]));
+
+    controller.set('store', this.store);
   }
 });
 
 test('it creates a new newsitem', function(assert) {
   var controller = this.subject();
-  controller.set('new_content', "a new post");
 
-  var spy = sinon.spy(store, "createRecord");
+  controller.set('new_content', "a new post");
 
   Ember.run(function(){
     controller.send('create');
   });
 
-  assert.ok(spy.calledOnce);
-  assert.ok(spy.calledWith('newsitem'));
+  assert.ok(this.store.createRecord.calledOnce);
+  assert.ok(this.store.createRecord.calledWith('newsitem'));
 });
 
 test('it persists the new newsitem to the server', function(assert) {
   var controller = this.subject();
   controller.set('new_content', "a new post");
-  var spy = sinon.spy(new_record, "save");
+
   Ember.run(function(){
     controller.send('create');
   });
-  assert.ok(spy.calledOnce);
+
+  assert.ok(this.new_record.save.calledOnce);
 });
 
 
 test('it clears the content field after posting', function(assert) {
   var controller = this.subject();
   controller.set('new_content', "another new post");
+
   Ember.run(function(){
     controller.send('create');
   });
+
   assert.equal(controller.get('new_content'), '');
 });
