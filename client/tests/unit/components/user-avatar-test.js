7886e4438dbaffb6f9f08f23dc4120757fcee4a1
Simon Olsberg <simon@olsbergfamily.net>
2015-08-14 19:15:09 -0400

    add component integration test for user-avatar

--- client/tests/unit/components/user-avatar-test.js
+++ client/tests/unit/components/user-avatar-test.js
@@ -1,31 +1,49 @@
 import { moduleForComponent, test } from 'ember-qunit';
-import Ember from 'ember';
+import hbs from 'htmlbars-inline-precompile';
+// import Ember from 'ember';
 
 moduleForComponent('user-avatar', 'Unit | Component | user avatar', {
   // Specify the other units that are required for this test
   // needs: ['component:foo', 'helper:bar']
-  unit: true,
-  
-  setup: function () {
-    this.registry.register('service:-routing', Ember.Object.extend({
-      availableRoutes: function() { return ['index']; },
-      hasRoute: function(name) { return name === 'index'; },
-      isActiveForRoute: function() { return true; },
-      generateURL: function() { return "/"; }
-    }));
-  }
+  integration: true,
+
+  // setup: function () {
+  //   this.registry.register('service:-routing', Ember.Object.extend({
+  //     availableRoutes: function() { return ['index']; },
+  //     hasRoute: function(name) { return name === 'index'; },
+  //     isActiveForRoute: function() { return true; },
+  //     generateURL: function() { return "/"; }
+  //   }));
+  // }
 });
 
 test('it renders', function(assert) {
-  assert.expect(2);
+  this.set('user', {
+    email: "user@example.com",
+    profile: {}
+  });
+  this.render(hbs`{{user-avatar user=this.user}}`);
 
-  // Creates the component instance
-  var component = this.subject();
-  assert.equal(component._state, 'preRender');
-
-  component.set('user', {email: "user@example.com"});
-
-  // Renders the component to the page
-  this.render();
-  assert.equal(component._state, 'inDOM');
+  assert.equal(this.$('a').length, 1);
 });
+
+test('it contains a gravatar image', function(assert) {
+  this.set('user', {
+    email: "user@example.com",
+    profile: {}
+  });
+  this.render(hbs`{{user-avatar user=this.user}}`);
+
+  assert.ok(this.$('a img').attr("src").match(/gravatar\.com/i));
+});
+
+// test('it links to user profile', function(assert) {
+//   this.set('user', {
+//     email: "user@example.com",
+//     username: 'thisuser',
+//     profile: {}
+//   });
+//   this.render(hbs`{{user-avatar user=this.user}}`);
+//   console.log("***" + this.$('a').parent()ph.html() + "???");
+//   assert.ok(this.$('a').attr("href").match(/\/members\/thisuser\/profile/i));
+// });
