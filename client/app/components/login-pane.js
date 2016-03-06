d7d173402ea2390e33dbfeb3dc8984217e45c933
Simon Olsberg <simon@olsbergfamily.net>
2016-03-06 12:29:56 -0500

    Place lock inside container on page rather than popup

--- client/app/components/login-pane.js
+++ client/app/components/login-pane.js
@@ -1,14 +1,15 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+export default Ember.Component.extend({
   session: Ember.inject.service(),
 
-  beforeModel: function() {
+  didRender: function(){
+    this._super(...arguments);
     var lockOptions = {
       closable: false,
+      container: 'login-pane',
       authParams: {scope: 'openid'}
     };
     this.get('session').authenticate('authenticator:lock', lockOptions);
   }
-
 });
